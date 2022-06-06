3d416c9ed98fe2b8e282cc04028284af74d96742
Andywambo <andrinepwambui@gmail.com>
2022-06-06 23:41:02 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,34 @@
-#include <stdio.h>
+#include "stdio.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * cap_string - a function that capitalizes all words of a string
+ * @n: input string
+ * Return: caps on first letter of a separator
  */
-int main(void)
+char *cap_string(char *n)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	int i, x;
+	int cap = 32;
+	int separators[] = {',', ';', '.', '?', '"',
+			    '(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - cap;
+		}
+
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == separators[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (n);
 }
