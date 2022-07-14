537ef64eda477d59759a5295729fab2d6e0e12be
Andywambo <andrinepwambui@gmail.com>
2022-07-14 17:42:20 +0300

    singly project

--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -1,34 +1,34 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
+ */
+
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
+
 /**
- * main - check the code for Holberton School students.
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Return: Always 0.
- */ int main(void) {
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (0);
-}
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* LISTS_H */
